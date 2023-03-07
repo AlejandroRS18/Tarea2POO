4a81faafdd9afd72dc6e6b29042094aaa031d13a
AlejandroRS18 <alex.ok.rincon@gmail.com>
2023-03-06 19:41:04 -0600

    Main

--- Apartamento.cs
+++ Apartamento.cs
@@ -2,12 +2,10 @@ namespace Tarea2POO;
 
 public class Apartamento : Casa
 {
-    // Constructor por defecto que llama al constructor de su clase padre con un valor de área de 50
     public Apartamento() : base(50)
     {
     }
 
-    // Método para mostrar los datos del apartamento
     public new void MostrarDatos()
     {
         Console.WriteLine($"Soy un Apartamento, mi área es {area} m2.");
--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Persona persona = new Persona();
+        persona.MostrarDatos();
+        persona.SetNombre("Carlos");
+        Apartamento apartamento = new Apartamento();
+        Casa casa = new Casa(50);
+        persona.SetCasa(casa);
+        persona.MostrarDatos();
+        //persona.casa.SetPuerta("Verde");
+
     }
 }
